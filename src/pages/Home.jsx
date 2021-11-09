13d14be309876016199dce65fc9054794e70bed2
jcsalinas20 <jcsalinasn20@gmail.com>
2021-11-09 23:57:07 +0100

    craete index Home page and instalations

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <p>fondo de estrellas(si puede ser algun efecto del raton)</p>
+                    <p>Presentacion ...</p>
+                    <p>boton para ir directamente a los proyectos</p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div>
+                    <p>My Skills</p>
+                    <p>About Me</p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div>
+                    <p>Git Stats</p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div>
+                    <p>My Projects</p>
+                    <p>Collaborations</p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div>
+                    <p>Contact With Me</p>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Home;
