eddb4b0b445e928d7cf3561c76f9aac54231af95
jcsalinas20 <jcsalinasn20@gmail.com>
2021-12-19 11:48:24 +0100

    new component Popup TargetProfile

---
+++ src/components/TargetProfile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Popup, Header, Icon, Image } from "semantic-ui-react";
+
+function TargetProfile(props) {
+    const url = props.data.url;
+    const username = props.data.username;
+    const name = props.data.name;
+    const avatar = props.data.avatar;
+    const location = props.data.location;
+    const company = props.data.company;
+    const blog = props.data.blog;
+    const twitter = props.data.twitter;
+
+    console.log(props);
+
+    return (
+        <Popup className="taget-profile" trigger={<a target="_blank" href={url}>{username}</a>} flowing hoverable inverted>
+            <Header as='h3'><Image src={avatar} circular />{name}</Header>
+            <hr />
+            <p><Icon name="map marker alternate" color="green" /><b>Location: </b> {location}</p>
+            <p><Icon name="building" color="violet" /><b>Company: </b> {company}</p>
+            <p><Icon name="globe" color="teal" /><b>Blog: </b> <a href="#">{blog}</a></p>
+            <p><Icon name="twitter" color="blue" /><b>Twitter: </b> <a target="_blank" href={"https://twitter.com/" + twitter}>{twitter}</a></p>
+        </Popup>
+    );
+}
+
+export default TargetProfile;
--- src/pages/GitStates.jsx
+++ src/pages/GitStates.jsx
@@ -1,16 +1,28 @@
+import React, { useEffect, useState } from "react";
 import { Header, Segment, Icon } from 'semantic-ui-react';
 
 import Loading from "../components/Loading";
+import TargetProfile from "../components/TargetProfile";
 
 function GitStates() {
+    const [user, setUser] = useState({});
+    // const [loading, setLoading] = useState(true);
+
+    useEffect(async () => {
+        setUser(await fetch("http://localhost:1337/api/git/user/get")
+            .then((res) => res.json())
+            .then((json) => { return json.user }));
+    }, []);
+
+
 
     return (
         <div className="git-states">
             <Segment color="blue" className="github-card">
-                <Loading color="white" />
+                {(!user.username) ? <Loading color="white" /> : ""}
                 <Header as='h1'>
                     <Icon name='github' inverted />
-                    Git States (<a href="https://github.com/jcsalinas20">jcsalinas20</a>)
+                    Git States (<TargetProfile data={user} />)
                 </Header>
             </Segment>
         </div>
