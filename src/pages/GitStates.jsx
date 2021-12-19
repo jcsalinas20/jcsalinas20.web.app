6858c638a1d53dd5edc9dda2be09dbb656b5be56
jcsalinas20 <jcsalinasn20@gmail.com>
2021-12-19 12:33:40 +0100

    Git States > Main Data

--- src/pages/GitStates.jsx
+++ src/pages/GitStates.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Header, Segment, Icon } from 'semantic-ui-react';
+import { Header, Segment, Icon, Image } from 'semantic-ui-react';
 
 import Loading from "../components/Loading";
 import TargetProfile from "../components/TargetProfile";
 
 function GitStates() {
     const [user, setUser] = useState({});
-    // const [loading, setLoading] = useState(true);
 
     useEffect(async () => {
         setUser(await fetch("http://localhost:1337/api/git/user/get")
             .then((res) => res.json())
             .then((json) => { return json.user }));
     }, []);
-
-
 
     return (
         <div className="git-states">
@@ -24,6 +21,17 @@ function GitStates() {
                     <Icon name='github' inverted />
                     Git States (<TargetProfile data={user} />)
                 </Header>
+                <Segment.Group horizontal raised className="main-data">
+                    <Segment className="data"><Image src={user.avatar} size="small" circular />{user.name}</Segment>
+                    <Segment className="data">{"Followers: " + user.followers}</Segment>
+                    <Segment className="data">{"Repositories: " + user.public_repos}</Segment>
+                    <Segment className="data">{"Collaborations: X"}</Segment>
+                    <Segment className="data">{"Stars: X"}</Segment>
+                </Segment.Group>
+                <Segment.Group horizontal raised>
+                    <Segment><Header as="h2">Organizations</Header></Segment>
+                    <Segment><Header as="h2">Languajes</Header></Segment>
+                </Segment.Group>
             </Segment>
         </div>
     );
